69f81e13ba859e16f172ce670a2a0fda852657bf
Tomasz Szpak <tomszp@gmail.com>
2025-12-28 23:57:45 +0100

    add base  subfolder

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
+    base: process.env.GITHUB_ACTIONS ? '/tomjan-eley/' : '/',
     i18n: {
         defaultLocale: "en",
         locales: ["en", "pl"],
